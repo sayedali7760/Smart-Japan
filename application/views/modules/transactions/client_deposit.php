41709e5e84789d1fad898065f550267624ab4a67
Seyad Ali Nisamudeen <sayed.ali7760@gmail.com>
2025-03-20 14:34:44 +0400

    20/03/2025

--- application/views/modules/transactions/client_deposit.php
+++ application/views/modules/transactions/client_deposit.php
@@ -46,8 +46,7 @@
 
                                 <div class="card-body pt-0">
                                     <div class="d-flex flex-wrap gap-5">
-
-                                        <div class="fv-row w-100 flex-md-root">
+                                        <div class="fv-row w-100 flex-md-root form_div">
                                             <label class="required form-label">Account</label>
                                             <select class="form-select mb-5" data-control="select2"
                                                 data-placeholder="Select an option"
@@ -62,7 +61,7 @@
                                                 ?>
                                             </select>
                                         </div>
-                                        <div class="fv-row w-100 flex-md-root">
+                                        <div class="fv-row w-100 flex-md-root form_div">
                                             <label class="required form-label">Deposit Method</label>
                                             <select class="form-select mb-5" data-control="select2"
                                                 data-placeholder="Select an option"
@@ -72,7 +71,7 @@
                                                 <option value="2">SticPay</option>
                                             </select>
                                         </div>
-                                        <div class="fv-row w-100 flex-md-root">
+                                        <div class="fv-row w-100 flex-md-root form_div">
                                             <label class="required form-label">Currency</label>
                                             <select class="form-select mb-5" data-control="select2"
                                                 data-placeholder="Select an option"
@@ -80,24 +79,38 @@
                                                 <option value="1">USD</option>
                                             </select>
                                         </div>
-
                                     </div>
-
-                                    <div class="d-flex flex-wrap gap-5">
-
-
-
-
-                                        <div class="fv-row w-100 flex-md-root">
+                                    <div class="gap-5 wallet_div" style="display: none;">
+                                        <div class="alert alert-primary d-flex align-items-center p-5 mb-10">
+                                            <!--begin::Svg Icon | path: icons/duotune/general/gen048.svg-->
+                                            <span class="svg-icon svg-icon-2hx svg-icon-primary me-4">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                    <path opacity="0.3" d="M20.5543 4.37824L12.1798 2.02473C12.0626 1.99176 11.9376 1.99176 11.8203 2.02473L3.44572 4.37824C3.18118 4.45258 3 4.6807 3 4.93945V13.569C3 14.6914 3.48509 15.8404 4.4417 16.984C5.17231 17.8575 6.18314 18.7345 7.446 19.5909C9.56752 21.0295 11.6566 21.912 11.7445 21.9488C11.8258 21.9829 11.9129 22 12.0001 22C12.0872 22 12.1744 21.983 12.2557 21.9488C12.3435 21.912 14.4326 21.0295 16.5541 19.5909C17.8169 18.7345 18.8277 17.8575 19.5584 16.984C20.515 15.8404 21 14.6914 21 13.569V4.93945C21 4.6807 20.8189 4.45258 20.5543 4.37824Z" fill="currentColor"></path>
+                                                    <path d="M10.5606 11.3042L9.57283 10.3018C9.28174 10.0065 8.80522 10.0065 8.51412 10.3018C8.22897 10.5912 8.22897 11.0559 8.51412 11.3452L10.4182 13.2773C10.8099 13.6747 11.451 13.6747 11.8427 13.2773L15.4859 9.58051C15.771 9.29117 15.771 8.82648 15.4859 8.53714C15.1948 8.24176 14.7183 8.24176 14.4272 8.53714L11.7002 11.3042C11.3869 11.6221 10.874 11.6221 10.5606 11.3042Z" fill="currentColor"></path>
+                                                </svg>
+                                            </span>
+                                            <!--end::Svg Icon-->
+                                            <div class="d-flex flex-column">
+                                                <h4 class="mb-1 text-primary">Warning</h4>
+                                                <span>This wallet address will disappear once you reload this page. Please make sure to copy and save it before refreshing or navigating away. Failure to do so may result in losing access to this address.</span>
+                                            </div>
                                         </div>
                                     </div>
+                                    <div class="gap-5 wallet_div" style="display: none;">
+                                        <p class="mb-1 text-success"><b>Wallet Address(TRC-20) - </b><span id="wallet_address" class="mb-5 text-primary" style="font-size: 19px;"></span>
+                                        </p>
+                                        <input type="hidden" name="wallet_input" id="wallet_input">
+                                    </div>
+
                                     <div class="d-flex justify-content-end">
 
-                                        <a href="<?php echo base_url(); ?>home" id="kt_ecommerce_add_product_cancel"
+                                        <a href="<?php echo base_url(); ?>transaction/deposit-client" id="kt_ecommerce_add_product_cancel"
                                             class="btn btn-light me-5">Cancel</a>
 
-                                        <a href="javascript:void(0);" class="btn btn-primary" title="Save Changes"
+                                        <a href="javascript:void(0);" class="btn btn-primary submit_butt" title="Save Changes"
                                             onclick="submit_data()">Deposit</a>
+
+                                        <a href="javascript:void(0);" style="display:none;" class="btn btn-success copy_butt" id="copyButton" title="Copy" onclick="copy_address()">Copy</a>
 
                                     </div>
 
@@ -117,6 +130,24 @@
     </div>
 </div>
 <script>
+    function copy_address() {
+        var walletText = $("#wallet_input").val();
+
+        var tempInput = $("<input>");
+        $("body").append(tempInput);
+        tempInput.val(walletText).select();
+        document.execCommand("copy");
+        tempInput.remove();
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Wallet address copied successfully! You can now paste it into any TRC-20 transfer application to proceed with your transaction safely. Make sure to double-check the address before confirming the transfer.',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK'
+        });
+    }
+
     function submit_data() {
         $("#loader").show();
         var ops_url = baseurl + 'transaction/deposit-save';
@@ -166,16 +197,19 @@
                 $("#loader").hide();
                 var data = $.parseJSON(result);
                 if (data.status == 1) {
+                    console.log(data.result_data);
+                    $('#wallet_address').html(data.wallet_address);
+                    $('#wallet_input').val(data.wallet_address);
+                    $('.wallet_div').show();
+                    $('.form_div').hide();
+                    $('.submit_butt').hide();
+                    $('.copy_butt').show();
                     Swal.fire({
                         icon: 'success',
                         title: 'Success',
-                        text: 'Deposit Completed.',
+                        text: 'Wallet address successfully generated. Please copy it and use any TRC-20 transfer application to complete the transaction.',
                         confirmButtonColor: '#3085d6',
                         confirmButtonText: 'OK'
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            location.reload();
-                        }
                     });
                 } else {
                     $('#faculty_loader').removeClass('sk-loading');
