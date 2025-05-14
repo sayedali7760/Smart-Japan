ca6dbd81c777e6affbdbd43a242b32f95a416c9c
Seyad Ali Nisamudeen <seyad.ali7760@gmail.com>
2025-05-14 11:04:30 +0400

    `Updated login/signup page links in application configuration and views.`

--- application/views/login/signup.php
+++ application/views/login/signup.php
@@ -53,7 +53,7 @@
                     <!--begin::Heading-->
                     <div class="text-center mb-10">
                         <!--begin::Title-->
-                        <a href="<?php echo base_url(); ?>../../demo8/dist/index.html" class="mb-12">
+                        <a href="<?php echo base_url(); ?>signup" class="mb-12">
                             <img alt="Logo" src="<?php echo base_url(); ?>assets/media/logos/Logo.png" class="h-40px" />
                         </a>
                         <!--end::Title-->
@@ -86,9 +86,9 @@
                     <div class="fv-row mb-10">
                         <label class="form-label fs-6 fw-bolder text-dark">Phone No</label>
                         <input type="text" class="form-control form-control-lg form-control-solid" autocomplete="off"
-                            onPaste="return false" placeholder="Enter Phone No" id="phno" name="phno" value="<?php if (isset($_COOKIE["ecomm_phno"])) {
-                                                                                                                    echo $_COOKIE["green_username"];
-                                                                                                                } ?>">
+                            onPaste="return false" placeholder="Enter Phone No" id="phno" name="phno" maxlength="8" value="<?php if (isset($_COOKIE["ecomm_phno"])) {
+                                                                                                                                echo $_COOKIE["green_username"];
+                                                                                                                            } ?>">
                     </div>
 
 
